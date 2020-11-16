80ef7040afa713b9aeb9102fdeaa367997560b9f
zempo <zelenkosolomon@gmail.com>
2020-11-16 12:29:23 -0800

    add user reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//redux imports
+import { applyMiddleware, compose, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+//modules
+import rootReducer from './reducers'
+
+//redux dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+//redux store
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
 
 ReactDOM.render(
   <React.StrictMode>
