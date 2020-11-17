3dbce3670f9cd012e35464444b40e5c5498c22d2
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-17 10:01:58 -0800

    added login and register routes

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Login () {
     return (
         <>
+        <h1>This is from the login</h1> 
         </>
     );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Register() {
     return (
         <> 
+        <h1>This is from register</h1>
         </>
     )
 }
