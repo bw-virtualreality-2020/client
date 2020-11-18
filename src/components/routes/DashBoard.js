5ac5ae266cda8d734b14ff01d1532d24b36aa042
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-18 09:16:53 -0800

    removed the dummy data in the Dashboard

--- src/components/routes/DashBoard.js
+++ src/components/routes/DashBoard.js
@@ -3,10 +3,6 @@ import React from 'react'
 function Dashboard() {
     return (
         <div>
-            <h1>Project Name</h1>
-            <img src="https://images.unsplash.com/photo-1605649494594-bc97fe5d86cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/>
-            <h3>Project Goal</h3>
-            <p>Project Description</p>
         </div>
     )
 }
