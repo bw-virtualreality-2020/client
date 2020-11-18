4e3509e449f9c323849f42608cd1e3e9f9a53fac
zempo <zelenkosolomon@gmail.com>
2020-11-18 15:19:49 -0800

    add projects page

---
+++ src/components/routes/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <section className='pg projects-pg'>
+            <h1>Projects</h1>
+        </section>
+    )
+}
+
+export default Projects
