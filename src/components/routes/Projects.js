0898bf3eef6f647a54de7dc0edde3173a3c9793b
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-18 15:29:40 -0800

    created projects components

--- src/components/routes/Projects.js
+++ src/components/routes/Projects.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
-function Project() {
+
+function Project(props) {
+
+    useEffect(()=>{
+        fetchUser();
+    },[])
+
+    const fetchUser = () => {
+        axios.get('https://bw-virtualreality-2020.herokuapp.com/api/projects')
+        .then(res => {
+            console.log(res); 
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
     return (
-        <>
-        <h1>This is where the projects live</h1>
-        </>
+        <div>
+        <h1>This is where the projects live!</h1>
+        {console.log(props.data)}
+        </div>
     );
 }
 
-export default Project;
+const mapStateToProps = (state) => {
+    return {
+        data: state.userAuthReducer
+    }
+}
+
+export default connect (mapStateToProps, {})(Project);
