6343ca371c3e32fb7539126444148f3f3fa3942c
zempo <zelenkosolomon@gmail.com>
2020-11-18 18:39:23 -0800

    add project reducer

---
+++ src/store/actions/addProject.js
@@ -0,0 +1,13 @@
+import { axiosWithAuth } from '../../components/utils/axiosWithAuth';
+// import { configs } from '../../config';
+
+export const START_LOGIN = "START_LOGIN";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_ERR = "LOGIN_ERR";
+
+const addProject = (info) => dispatch => {
+    axiosWithAuth()
+    .then()
+}
+
+export default addProject
